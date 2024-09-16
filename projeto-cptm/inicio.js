91fa907a75ac6d57438e3583632c70557915d25f
ana <annnasantos4530@gmail.com>
2024-09-16 16:10:19 -0300

    Add files via upload

---
+++ projeto-cptm/inicio.js
@@ -0,0 +1,5 @@
+function addNotification() {
+    const notificationDiv = document.getElementById('notifications');
+notificationDiv.style.backgroundColor = '#f8d7da';
+    notificationDiv.innerHTML = '<p><strong>Aviso:</strong> Interrupção temporária na linha 7-Rubi</p>';
+}
